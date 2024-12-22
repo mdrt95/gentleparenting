fbad4d551c75de4fad8a34525d9be63886275ff6
mdrt95 <reyesmdtor@gmail.com>
2024-12-21 21:37:11 -0600

    added home screen background

--- app/src/main/java/com/example/gpa/screens/HomeScreen.kt
+++ app/src/main/java/com/example/gpa/screens/HomeScreen.kt
@@ -1,7 +1,9 @@
 package com.example.gpa.screens
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -14,20 +16,36 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.gpa.R
 import com.example.gpa.components.LearningPath
 
 @Composable
 fun HomeScreen() {
     //var streakCount by remember { mutableStateOf(0) }
     //var points by remember { mutableStateOf(0) }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ){
+        Image(
+            painter = painterResource(R.drawable.gpa_home_background),
+            contentDescription = "Home Screen Background",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxSize()
 
+        )
+    }
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp)
-            .background(Color.Gray),
+            .background(Color.Transparent),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Row(
